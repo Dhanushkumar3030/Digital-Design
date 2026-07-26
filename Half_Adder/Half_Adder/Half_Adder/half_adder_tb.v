f23c3b5f86e2aed63b67d06897a2121dd9f28741
DhanushKumar <dhanushkumar.u.3030@gmail.com>
2026-07-26 11:37:10 +0530

    Update half_adder_tb.v

--- Half_Adder/Half_Adder/Half_Adder/half_adder_tb.v
+++ Half_Adder/Half_Adder/Half_Adder/half_adder_tb.v
@@ -1,1 +1,23 @@
 
+module half_adder_tb;
+
+    reg a, b;
+    wire s, c;
+    integer i;
+
+    // DUT Instantiation
+    half_adder dut(a, b, s, c);
+
+    initial begin
+        $display("A B | S C");
+        $monitor("%b %b | %b %b", a, b, s, c);
+
+        for(i = 0; i < 4; i = i + 1) begin
+            {a, b} = i;
+            #10;
+        end
+
+        $finish;
+    end
+
+endmodule
